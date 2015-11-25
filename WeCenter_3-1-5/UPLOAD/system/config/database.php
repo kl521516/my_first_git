064ee752e69baf0871ef9d7c377fab6766bc5245
kl521516 <kl521516@126.com>
2015-11-25 16:53:29 +0800

    add wecenter

---
+++ WeCenter_3-1-5/UPLOAD/system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => '521516123',
+  'dbname' => 'wecenter_315',
+);
+$config['slave'] = false;
